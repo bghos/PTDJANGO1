91c428a360f79f559ae47bfa64f417d4aedded04
bghos <bghosal1987@gmail.com>
2025-01-05 02:20:47 -0700

    Done

---
+++ djangofirst.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from rest_framework import viewsets
+from rest_framework import routers
+from rest_framework.response import Response
+from rest_framework import decorators
+
+
+
